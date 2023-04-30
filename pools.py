c45c741fdf5c771b2091176db81f8ea2bea63738
BlackData <96271871+blackdatadao@users.noreply.github.com>
2023-04-30 20:07:56 +0800

    Update pools.py

--- pools.py
+++ pools.py
@@ -43,7 +43,7 @@ df4['create_time']=df4['create_time'].map(lambda x:datetime.strptime(x,'%Y-%m-%d
 
 for index,row in df4.iterrows():
     with st.container():
-        st.markdown('**NFT ID:** '+str(row['nft_id'])+' **Symbol:** '+row['symbol0']+'/'+row['symbol1']+' **Tick:** '+str(row['tick_lower'])+'-'+str(row['tick_upper'])+' **Fee:** '+str(row['fee_usdc'])+' **Value:** '+str(row['value'])+' **Duration:** '+str(row['duration'])+' **Create Time:** '+row['create_time'])    
+        st.markdown('**ID** '+str(row['nft_id'])+' ** ** '+row['symbol0']+'/'+row['symbol1']+' **** '+str(row['tick_lower'])+'-'+str(row['tick_upper'])+' **FeeUSDC:** '+str(row['fee_usdc'])+' **Value:** '+str(row['value'])+' **Duration:** '+str(row['duration'])+' **** '+row['create_time'])    
         st.markdown('**Token0:** '+str(row['create_token0'])+' **Token1:** '+str(row['create_token1'])+' **Token0 Withdrawable:** '+str(row['withdrawable_tokens0'])+' **Token1 Withdrawable:** '+str(row['withdrawable_tokens1']))   
         st.write('---')
 
