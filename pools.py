e1658c7ffb2f31385fc640e40ead765cb22e6484
BlackData <96271871+blackdatadao@users.noreply.github.com>
2024-02-04 21:44:47 +0800

    Update pools.py

--- pools.py
+++ pools.py
@@ -378,7 +378,39 @@ fig.update_layout(title='ETH/arb price')
 st.plotly_chart(fig, use_container_width=True
                 )
 
+import logging
+from binance.spot import Spot as Client
+from binance.lib.utils import config_logging
+import pandas as pd
+from datetime import datetime
+import plotly.graph_objects as go
 
+config_logging(logging, logging.DEBUG)
+
+spot_client = Client(base_url="https://testnet.binance.vision")
+
+# logging.info(spot_client.klines("BTCUSDT", "1m"))
+x=spot_client.klines("ARBETH", "4h", limit=10)
+#convert to dateframe
+
+df=pd.DataFrame(x)
+df.columns=['Open Time','Open','High','Low','Close','Volume','Close Time','Quote Asset Volume','Number of Trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore']
+df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
+df['Close Time'] = pd.to_datetime(df['Close Time'], unit='ms')
+#convert price data to 1/the actual price
+df['Open']=1/df['Open'].astype(float)
+df['High']=1/df['High'].astype(float)
+df['Low']=1/df['Low'].astype(float)
+df['Close']=1/df['Close'].astype(float)
+#create a candlestick chart
+
+fig = go.Figure(data=[go.Candlestick(x=df['Open Time'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+st.plotly_chart(fig, use_container_width=True
+                )
 
 df=get_history_price_ethusdc(w3)
 timestamp_start=w3.eth.get_block(int(df['blockNumber'][0]))['timestamp']
