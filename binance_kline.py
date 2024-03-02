6c0aa3a743f91dba7d9a76d687b968399b4e0ce2
Your Name <your.email@example.com>
2024-03-02 17:05:53 +0800

    add compare chart

--- binance_kline.py
+++ binance_kline.py
@@ -3,16 +3,38 @@
 import logging
 from binance.spot import Spot as Client
 from binance.lib.utils import config_logging
+import pandas as pd
 from datetime import datetime
+import plotly.graph_objects as go
 
 config_logging(logging, logging.DEBUG)
+
+def get_kline_data_from_binance(symbol, interval, limit):
+    spot_client = Client(base_url="https://testnet.binance.vision")
+    kline_data = spot_client.klines(symbol, interval, limit=limit)
+    kline_data = pd.DataFrame(kline_data)
+    kline_data.columns = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number of Trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
+    kline_data['Open Time'] = pd.to_datetime(kline_data['Open Time'], unit='ms')
+    kline_data['Close Time'] = pd.to_datetime(kline_data['Close Time'], unit='ms')
+    kline_data['Open'] = kline_data['Open'].astype(float)
+    kline_data['High'] = kline_data['High'].astype(float)
+    kline_data['Low'] = kline_data['Low'].astype(float)
+    kline_data['Close'] = kline_data['Close'].astype(float)
+    return kline_data
+
+def reverse_price(kline_data):
+    kline_data['Open'] = 1 / kline_data['Open']
+    kline_data['High'] = 1 / kline_data['High']
+    kline_data['Low'] = 1 / kline_data['Low']
+    kline_data['Close'] = 1 / kline_data['Close']
+    return kline_data
 
 spot_client = Client(base_url="https://testnet.binance.vision")
 
 # logging.info(spot_client.klines("BTCUSDT", "1m"))
 x=spot_client.klines("ARBETH", "4h", limit=10)
 #convert to dateframe
-import pandas as pd
+
 df=pd.DataFrame(x)
 df.columns=['Open Time','Open','High','Low','Close','Volume','Close Time','Quote Asset Volume','Number of Trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore']
 df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
@@ -23,7 +45,7 @@ df['High']=1/df['High'].astype(float)
 df['Low']=1/df['Low'].astype(float)
 df['Close']=1/df['Close'].astype(float)
 #create a candlestick chart
-import plotly.graph_objects as go
+
 fig = go.Figure(data=[go.Candlestick(x=df['Open Time'],
                 open=df['Open'],
                 high=df['High'],
@@ -31,6 +53,8 @@ fig = go.Figure(data=[go.Candlestick(x=df['Open Time'],
                 close=df['Close'])])
 #show the fig
 fig.show()
+
+
 
 c=1
 
