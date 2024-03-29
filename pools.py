49e22f8bf63c8805db247b94de03ce1a58eb1415
Your Name <your.email@example.com>
2024-03-29 22:49:37 +0800

    add realised

--- pools.py
+++ pools.py
@@ -311,6 +311,29 @@ def get_summary(df):
     df_summary.columns=['w0','w1','c0','c1','fee','value','delta0','delta1','cost']
     return df_summary
 
+def send_id_to_server(user_input):
+    url = 'http://42.192.17.155/realised_id'
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        # GET request to fetch the current list
+        response = requests.get(url)
+        if response.status_code == 200:
+                data = response.json()
+                data.append(user_input)
+                # POST request to send the updated list back to the server
+                post_response = requests.post(url, json=data, headers=headers)
+                post_response.raise_for_status()  # Check for HTTP errors
+                st.success("Data successfully sent to server.")
+        else:
+            st.error('Error:', response.status_code)
+    except json.JSONDecodeError:
+        st.error('Error: Invalid JSON')
+    except requests.exceptions.HTTPError as e:
+        st.error('HTTP error occurred:', e)
+    except requests.exceptions.RequestException as e:
+        st.error('Request failed:', e)
+
 # summary tabel 
 nft_data = get_pools_details(df)
 df3=pd.DataFrame(nft_data)
@@ -353,29 +376,11 @@ for index,row in df4.iterrows():
 
 
 
-# Streamlit UI to input an integer
-user_input = st.number_input('Enter an integer', step=1, format='%d')
+# Streamlit UI to input realised ID
+user_input = st.number_input('Enter realised id', step=1, format='%d')
+send_id_to_server(user_input)
+st.success('realised ID saved successfully!')
 
-# Button to append the input to the list and save it
-if st.button('Save Integer'):
-    json_file_path = 'realized_id.json'
-
-    if os.path.exists(json_file_path):
-        # Open the JSON file and load the data
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-    else:
-        # If the file does not exist, start with an empty list
-        data = []
-    # Append the user input to the list
-    data.append(user_input)
-    
-    # Open the JSON file in write mode and save the data
-    with open(json_file_path, 'w') as file:
-        json.dump(data, file)
-        
-    st.success('realised ID saved successfully!')
-    
 
 #plot the history price of etharb and ethusdc
 df=get_history_price_etharb(w3)
--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-import time
-def timestamp_to_time(timestamp):
-    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(timestamp))
+import time,requests,json
 
-print('0x'+hex(1264785)[2:].rjust(64,'0'))
-print(timestamp_to_time(1711634236))
+user_input = 0
+
+# Button to append the input to the list and save it
+def send_id_to_server(user_input):
+    url = 'http://42.192.17.155/realised_id'
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        # GET request to fetch the current list
+        response = requests.get(url)
+        if response.status_code == 200:
+                data = response.json()
+                data.append(user_input)
+                # POST request to send the updated list back to the server
+                post_response = requests.post(url, json=data, headers=headers)
+                post_response.raise_for_status()  # Check for HTTP errors
+                print("Data successfully sent to server.")
+        else:
+            print('Error:', response.status_code)
+    except json.JSONDecodeError:
+        print('Error: Invalid JSON')
+    except requests.exceptions.HTTPError as e:
+        print('HTTP error occurred:', e)
+    except requests.exceptions.RequestException as e:
+        print('Request failed:', e)
+
+c=1
